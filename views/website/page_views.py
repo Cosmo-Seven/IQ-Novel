a920be5d05beee5e3f80deab90affdc5a489b06c
o-PyiSoeKyaw-o <pyisoekyaw0@gmail.com>
2026-06-28 16:58:03 +0630

    update home page layout

--- views/website/page_views.py
+++ views/website/page_views.py
@@ -51,7 +51,7 @@ def index(request):
     completed_novels = NovelModel.objects.select_related("author").filter(is_completed=True)
     popular_novels = NovelModel.objects.all().order_by("-views")[:10]
     fanfic_novels = NovelModel.objects.select_related("author").filter(novel_type='fanfic')
-    free_novels = NovelModel.objects.select_related("author").filter(chapters__is_free=True).distinct()
+    free_novels = NovelModel.objects.select_related("author").filter(chapters__is_free=True).exclude(chapters__is_free=False).distinct()
     genres = GenreModel.objects.all()
 
     context = {
